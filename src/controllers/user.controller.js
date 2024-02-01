ce1d10a81e27b0ff1e23db8a74e859fa07f89b15
jenilmangukiya <jenilmangukiya2002@gmail.com>
2024-02-01 22:46:11 +0530

    [main] login logout API

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,28 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { User } from "../models/user.model.js";
 
+const cookiesOption = {
+  httpOnly: true,
+  secure: true,
+};
+
+const generateAccessAndRefreshToken = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Something went wrong while generating access and refresh token"
+    );
+  }
+};
+
 export const registerUser = asyncHandler(async (req, res) => {
   const { fullName, email, password } = req.body;
 
@@ -36,4 +58,56 @@ export const registerUser = asyncHandler(async (req, res) => {
   res
     .status(200)
     .json(new ApiResponse(200, createdUser, "User registered Successfully"));
+});
+
+export const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new ApiError(400, "Email and password are required!");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new ApiError(400, "Username or password is invalid");
+  }
+
+  const isUserValid = await user.isPasswordCorrect(password);
+
+  if (!isUserValid) {
+    throw new ApiError(400, "Please enter valid password");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+    user._id
+  );
+
+  const validUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  res
+    .status(200)
+    .cookie("accessToken", accessToken, cookiesOption)
+    .cookie("refreshToken", refreshToken, cookiesOption)
+    .json(
+      new ApiResponse(
+        200,
+        { user: validUser, accessToken, refreshToken },
+        "User logged in successfully"
+      )
+    );
+});
+
+export const logout = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(req.user._id, {
+    $unset: { refreshToken: 1 },
+  });
+
+  res
+    .status(200)
+    .clearCookie("accessToken", cookiesOption)
+    .clearCookie("refreshToken", cookiesOption)
+    .json(new ApiResponse(200, {}, "Logout success"));
 });
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,15 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
-
+import {
+  loginUser,
+  logout,
+  registerUser,
+} from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
+
+router.route("/logout").post(verifyJWT, logout);
 
 export default router;
